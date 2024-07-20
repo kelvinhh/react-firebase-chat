cd80b533675e2cc1f1ead6e7c85cebb584ac8978
kelvinhh <k11vinppp@gmail.com>
2024-07-20 17:18:03 +0800

    Remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,47 @@ import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import useUserStore from "./lib/userStore";
 import useChatStore from "./lib/chatStore";
 
 const App = () => {
-  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
-  const { chatId } = useChatStore();
+    const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+    const { chatId } = useChatStore();
 
-  useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user) => {
-      fetchUserInfo(user?.uid);
-    });
+    useEffect(() => {
+        const unSub = onAuthStateChanged(auth, (user) => {
+            fetchUserInfo(user?.uid);
+        });
 
-    return () => {
-      unSub();
+        return () => {
+            unSub();
+        }
+    }, [fetchUserInfo]);
+
+    if (isLoading) {
+        return (
+            <div className="loading">Loading...</div>
+        )
     }
-  }, [fetchUserInfo]);
 
-  if (isLoading) {
     return (
-      <div className="loading">Loading...</div>
+        <div className="container">
+            {
+                currentUser ? (
+                    <>
+                        <List />
+                        {chatId && <Chat />}
+                        <Detail />
+                    </>
+                ) :
+                    (<Login />)
+            }
+            <Notification />
+        </div>
     )
-  }
-
-  return (
-    <div className="container">
-      {
-        currentUser ? (
-          <>
-            <List />
-            {chatId && <Chat />}
-            <Detail />
-          </>
-        ) :
-          (<Login />)
-      }
-      <Notification />
-    </div>
-  )
 }
 
 export default App;
