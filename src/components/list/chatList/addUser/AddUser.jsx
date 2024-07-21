153995e811e7845110ec7bac61edfa8c1cf769ee
kelvinhh <k11vinppp@gmail.com>
2024-07-21 16:44:34 +0800

    prevent multiple addition to same user

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -2,7 +2,7 @@ import { db } from '../../../../lib/firebase';
 import './addUser.css'
 import { useState } from 'react';
 import useUserStore from '../../../../lib/userStore'
-import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
+import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where, getDoc } from 'firebase/firestore';
 
 const AddUser = () => {
     const [user, setUser] = useState(null);
@@ -32,6 +32,22 @@ const AddUser = () => {
         const userChatsRef = collection(db, "userchats");
 
         try {
+            const userChatsDoc = await getDoc(doc(userChatsRef, user.id));
+            const currentUserChatsDoc = await getDoc(doc(userChatsRef, currentUser.id));
+
+            if (userChatsDoc.exists() && currentUserChatsDoc.exists()) {
+                const userChats = userChatsDoc.data().chats || [];
+                const currentUserChats = currentUserChatsDoc.data().chats || [];
+
+                const chatExists = userChats.some(chat => chat.receiverId === currentUser.id) ||
+                    currentUserChats.some(chat => chat.receiverId === user.id);
+
+                if (chatExists) {
+                    console.log("Chat already exists. Skipping creation.");
+                    return;
+                }
+            }
+
             const newChatsRef = doc(chatsRef);
 
             await setDoc(newChatsRef, {
